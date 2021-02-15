768a0f1ae672a5c99ca4e69f6121437013596412
Vladislav <v.onipko.v@gmail.com>
2021-02-15 14:19:36 +0200

    commited

--- scripts/app.js
+++ scripts/app.js
@@ -46,15 +46,11 @@ class Card {
 }
 
 class Computer {
-  computerCards = document.querySelector('.cards');
 
-  constructor() {
-
-  }
-
-  putCard() {
-  
-    
+  putCard(position) {
+    const img = '<img src="./images/deck.jpg" alt="card">';
+    const card = document.querySelector(`.cards__list :nth-child(${position})`);
+    card.innerHTML = img;
   }
 }
 
@@ -62,19 +58,59 @@ class Score {
   playerScoreElement = document.getElementById('user').querySelector('p');
   computerScoreElement = document.getElementById('computer').querySelector('p');
 
-  calcScore(playerValue, computerValue) {
+  checkWinner(playerScore, computerScore) {
+    if (playerScore > 21 && computerScore <= 21) {
+      document.getElementById('lost').classList.add('show');
+      document.querySelector('.backdrop').classList.add('open');
+      setTimeout(() => {
+        document.getElementById('lost').classList.remove('show');
+        document.querySelector('.backdrop').classList.remove('open');
+        Game.newGame();
+      }, 3000)
+
+    } else if (playerScore <= 21 && computerScore > 21) {
+      document.getElementById('won').classList.add('show');
+      document.querySelector('.backdrop').classList.add('open');
+      setTimeout(() => {
+        document.querySelector('.backdrop').classList.remove('open');
+        document.getElementById('won').classList.remove('show');
+        Game.newGame();
+      }, 3000)
+
+    } else if (playerScore >= 21 && computerScore >= 21) {
+      document.getElementById('draw').classList.add('show');
+      document.querySelector('.backdrop').classList.add('open');
+      setTimeout(() => {
+        document.querySelector('.backdrop').classList.remove('open');
+        document.getElementById('draw').classList.remove('show');
+        Game.newGame();
+      }, 3000)
+    }
+  }
+
+  calcScore(playerCard, computerCard) {
     let currentPlayerScore = +this.playerScoreElement.textContent;
     let currentComputerScore = +this.computerScoreElement.textContent;
 
+    const score = {
+      player: currentPlayerScore + playerCard,
+      computer: currentComputerScore + computerCard,
+    };
+
     if (currentPlayerScore < 21 && currentComputerScore < 21) {
-      this.playerScoreElement.textContent = currentPlayerScore + playerValue;
-      this.computerScoreElement.textContent = currentComputerScore + computerValue;
+      this.playerScoreElement.textContent = score.player;
+      this.computerScoreElement.textContent = score.computer;
+
+    } else {
+      this.checkWinner(score.player, score.computer);
     }
+    return score;
   }
 }
 
 class Player extends Deck {
   playerCards = document.querySelector('.cards').nextElementSibling;
+  _position = 1;
 
   constructor() {
     super();
@@ -122,7 +158,7 @@ class Player extends Deck {
   connectDroppable() {
    this.playerCards.addEventListener('dragenter', event => {
     const target = this.getCardTarget(event);
-    if (target) target.classList.add('droppable');
+    if (target && !target.textContent) target.classList.add('droppable');
     event.preventDefault();
    });
 
@@ -139,21 +175,36 @@ class Player extends Deck {
 
    this.playerCards.addEventListener('drop', event => {
     const score = new Score();
+    const computer = new Computer();
     const playerCard = this.takeCard();
     const computerCard = this.takeCard();
     const target = this.getCardTarget(event);
-
-    target.classList.remove('droppable');
-    this.drowCard(playerCard, event);
-    score.calcScore(+playerCard.value, +computerCard.value);
     
+
+    if (target && !target.textContent) {
+      score.calcScore(+playerCard.value, +computerCard.value);
+    }
+
+    computer.putCard(this._position++);
+    this.drowCard(playerCard, event);
+    target.classList.remove('droppable');
    });
   }
 }
 
 class Game {
+
+  static newGame() {
+    const computerCards = Array.from(document.querySelector('.cards').querySelectorAll('li'));
+    const playerCards = Array.from(document.querySelector('.cards').nextElementSibling.querySelectorAll('li'));
+
+    computerCards.forEach(card => card.innerHTML = '');
+    playerCards.forEach(card => card.innerHTML = '');
+    document.getElementById('user').querySelector('p').textContent = 0;
+    document.getElementById('computer').querySelector('p').textContent = 0;
+  } 
+
   static play() {
-    new Deck();
     new Player();
   }
 }
